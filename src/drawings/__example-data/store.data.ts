a888c67d42a257ca7e208b9c438098e756566872
Dennis Zimmermann <dennzimm@icloud.com>
2020-07-08 16:40:44 +0200

    Add example data

---
+++ src/drawings/__example-data/store.data.ts
@@ -0,0 +1,26 @@
+// import { Injectable } from '@nestjs/common';
+// import {
+//   Drawings,
+//   Groups,
+//   GroupsData,
+//   Items,
+//   ItemsData,
+//   Layers,
+//   LayersData,
+// } from '../interfaces';
+// import * as dummyData from './example.data';
+
+// const DUMMY_DATA_MODE = true;
+
+// @Injectable()
+// export class DataStore {
+//   readonly drawings: Drawings = DUMMY_DATA_MODE ? dummyData.drawings : {};
+//   readonly layers: Layers = DUMMY_DATA_MODE ? dummyData.layers : {};
+//   readonly layersData: LayersData = DUMMY_DATA_MODE ? dummyData.layersData : {};
+//   readonly groups: Groups = DUMMY_DATA_MODE ? dummyData.groups : {};
+//   readonly groupsData: GroupsData = DUMMY_DATA_MODE ? dummyData.groupsData : {};
+//   readonly items: Items = DUMMY_DATA_MODE ? dummyData.items : {};
+//   readonly itemsData: ItemsData = DUMMY_DATA_MODE ? dummyData.itemsData : {};
+
+//   readonly defaultFailureValue = null;
+// }
