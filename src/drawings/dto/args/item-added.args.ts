46a280c54d1614d2b83ae4acb824beaafbef1141
Dennis Zimmermann <dennzimm@icloud.com>
2020-07-27 15:27:35 +0200

    Remove unnecessary dto defs

--- src/drawings/dto/args/item-added.args.ts
+++
@@ -1,5 +0,0 @@
-import { ArgsType } from '@nestjs/graphql';
-import { CommonSubscription } from '../../common/dto/common-subscription.dto';
-
-@ArgsType()
-export class ItemAddedArgs extends CommonSubscription {}
