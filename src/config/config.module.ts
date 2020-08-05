fc8c0d45f2602132150567693addfe981a140992
Dennis Zimmermann <dennzimm@icloud.com>
2020-08-05 13:27:55 +0200

    Add Redis pubsub

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -7,12 +7,14 @@ import { ConfigModule as NestConfigModule } from '@nestjs/config';
     NestConfigModule.forRoot({
       isGlobal: true,
       validationSchema: Joi.object({
+        SERVER_PORT: Joi.number(),
         POSTGRES_HOST: Joi.string().required(),
         POSTGRES_PORT: Joi.number().required(),
         POSTGRES_USER: Joi.string().required(),
         POSTGRES_PASSWORD: Joi.string().required(),
         POSTGRES_DB: Joi.string().required(),
-        SERVER_PORT: Joi.number(),
+        REDIS_HOST: Joi.string().required(),
+        REDIS_PORT: Joi.number().required(),
       }),
     }),
   ],
--- src/drawings/drawings.module.ts
+++ src/drawings/drawings.module.ts
@@ -1,18 +1,31 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { PubSub } from 'apollo-server-express';
+import { RedisPubSub } from 'graphql-redis-subscriptions';
+import * as Redis from 'ioredis';
 import Drawing from './entities/drawing.entity';
 import Item from './entities/item.entity';
 import { resolvers } from './resolvers';
 import { services } from './services';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Drawing, Item])],
   controllers: [],
   providers: [
     {
+      inject: [ConfigService],
       provide: 'PUB_SUB',
-      useValue: new PubSub(),
+      useFactory: (configService: ConfigService) => {
+        const options = {
+          host: configService.get('REDIS_HOST'),
+          port: configService.get('REDIS_PORT'),
+        };
+
+        return new RedisPubSub({
+          publisher: new Redis(options),
+          subscriber: new Redis(options),
+        });
+      },
     },
     ...services,
     ...resolvers,
