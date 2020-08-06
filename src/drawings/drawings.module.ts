2e6d0b86c663e121aec1053bc6f7f1f853533487
Dennis Zimmermann <dennzimm@icloud.com>
2020-08-06 14:25:32 +0200

    Remove redis (temporarily)

--- src/drawings/drawings.module.ts
+++ src/drawings/drawings.module.ts
@@ -1,31 +1,36 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { RedisPubSub } from 'graphql-redis-subscriptions';
-import * as Redis from 'ioredis';
+// import { ConfigService } from '@nestjs/config';
+import { PubSub } from 'apollo-server-express';
+// import { RedisPubSub } from 'graphql-redis-subscriptions';
+// import * as Redis from 'ioredis';
 import Drawing from './entities/drawing.entity';
 import Item from './entities/item.entity';
 import { resolvers } from './resolvers';
 import { services } from './services';
-import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Drawing, Item])],
   controllers: [],
   providers: [
-    {
-      inject: [ConfigService],
-      provide: 'PUB_SUB',
-      useFactory: (configService: ConfigService) => {
-        const options = {
-          host: configService.get('REDIS_HOST'),
-          port: configService.get('REDIS_PORT'),
-        };
+    // {
+    //   inject: [ConfigService],
+    //   provide: 'PUB_SUB',
+    //   useFactory: (configService: ConfigService) => {
+    //     const options = {
+    //       host: configService.get('REDIS_HOST'),
+    //       port: configService.get('REDIS_PORT'),
+    //     };
 
-        return new RedisPubSub({
-          publisher: new Redis(options),
-          subscriber: new Redis(options),
-        });
-      },
+    //     return new RedisPubSub({
+    //       publisher: new Redis(options),
+    //       subscriber: new Redis(options),
+    //     });
+    //   },
+    // },
+    {
+      provide: 'PUB_SUB',
+      useValue: new PubSub(),
     },
     ...services,
     ...resolvers,
