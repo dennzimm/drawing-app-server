e793ffc504cd844d9ad96e44a5b16a582e771f35
Dennis Zimmermann <dennzimm@icloud.com>
2020-08-06 14:25:02 +0200

    Update types for segment model

--- src/drawings/models/segment.model.ts
+++ src/drawings/models/segment.model.ts
@@ -4,28 +4,28 @@ import { CommonDrawingInput } from '../common/dto/common-drawing.dto';
 @InputType('SegmentDataInput')
 @ObjectType()
 export class SegmentData {
-  @Field(type => Float)
+  @Field((type) => Float)
   x: number;
 
-  @Field(type => Float)
+  @Field((type) => Float)
   y: number;
 }
 
 @InputType('NewSegmentInput')
 @ObjectType()
 export class Segment extends CommonDrawingInput {
-  @Field(type => ID)
+  @Field((type) => ID)
   itemID: string;
 
-  @Field(type => SegmentData)
+  @Field((type) => SegmentData)
   segmentData: SegmentData;
 
-  @Field(type => String, { nullable: true })
+  @Field((type) => String, { nullable: true })
   strokeColor?: string;
 
-  @Field(type => [Float], { nullable: true })
-  fillColor?: number[];
+  @Field((type) => String, { nullable: true })
+  fillColor?: string;
 
-  @Field(type => Int, { nullable: true })
+  @Field((type) => Float, { nullable: true })
   strokeWidth?: number;
 }
