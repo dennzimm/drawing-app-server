c3ee9f9caa18827124ffb55eda3e983c5cdf1f12
Dennis Zimmermann <dennzimm@icloud.com>
2020-08-14 19:20:56 +0200

    [WIP] Add clients tracker for subscriptions

--- src/graphql/graphql.module.ts
+++ src/graphql/graphql.module.ts
@@ -1,6 +1,13 @@
-import { Module } from '@nestjs/common';
+import { ForbiddenException, Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
+
+interface ConnectionParams extends Object {
+  userID: string;
+  drawingID: string;
+}
+
+const clients = new Map<any, ConnectionParams>();
 
 @Module({
   imports: [
@@ -8,6 +15,29 @@ import { join } from 'path';
       context: ({ req }) => ({ req }),
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       installSubscriptionHandlers: true,
+      subscriptions: {
+        onConnect: (connectionParams: ConnectionParams, websocket, context) => {
+          const { userID, drawingID } = connectionParams;
+
+          if (!userID || !drawingID) {
+            throw new ForbiddenException(
+              'userID and drawingID must be supplied as connectionParams',
+            );
+          }
+
+          clients.set(websocket, {
+            userID,
+            drawingID,
+          });
+
+          console.log(`user ${userID} connected to drawing ${drawingID}`);
+        },
+        onDisconnect: (websocket, context) => {
+          const { userID, drawingID } = clients.get(websocket);
+          console.log(`user ${userID} disconnected from drawing ${drawingID}`);
+          clients.delete(websocket);
+        },
+      },
     }),
   ],
 })
