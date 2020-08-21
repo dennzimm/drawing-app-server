f3ea4362019c5c2f43f96bd203ae6e7dd8e49f23
Dennis Zimmermann <dennzimm@icloud.com>
2020-08-21 10:35:54 +0200

    Add cirlce ci

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as compression from 'compression';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+// import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,24 +15,24 @@ async function bootstrap() {
   }
 
   // Swagger Api
-  if (process.env.SWAGGER_ENABLE === '1') {
-    const options = new DocumentBuilder()
-      .setTitle(process.env.SWAGGER_TITLE || 'Nestjs')
-      .setDescription(
-        process.env.SWAGGER_DESCRIPTION || 'The nestjs API description',
-      )
-      .setVersion(process.env.SWAGGER_VERSION || '1.0')
-      .build();
-    const document = SwaggerModule.createDocument(app, options);
+  // if (process.env.SWAGGER_ENABLE === '1') {
+  //   const options = new DocumentBuilder()
+  //     .setTitle(process.env.SWAGGER_TITLE || 'Nestjs')
+  //     .setDescription(
+  //       process.env.SWAGGER_DESCRIPTION || 'The nestjs API description',
+  //     )
+  //     .setVersion(process.env.SWAGGER_VERSION || '1.0')
+  //     .build();
+  //   const document = SwaggerModule.createDocument(app, options);
 
-    SwaggerModule.setup(process.env.SWAGGER_PATH || 'api', app, document);
-  }
+  //   SwaggerModule.setup(process.env.SWAGGER_PATH || 'api', app, document);
+  // }
 
   // Cors
   if (process.env.CORS_ENABLE === '1') {
     app.enableCors();
   }
 
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
