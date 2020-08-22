b69b791d38fa1607a806edc6322f10d4ab631518
Dennis Zimmermann <dennzimm@icloud.com>
2020-08-22 16:31:19 +0200

    Update redis pubsub

--- src/pubsub/pubsub.provider.ts
+++ src/pubsub/pubsub.provider.ts
@@ -1,24 +1,28 @@
-// import { RedisPubSub } from 'graphql-redis-subscriptions';
-// import * as Redis from 'ioredis';
+import { RedisPubSub } from 'graphql-redis-subscriptions';
+import * as Redis from 'ioredis';
 import { PubSub } from 'apollo-server-express';
+import { ConfigService } from '@nestjs/config';
 
 export const PubSubProvider = {
-  provide: 'PUB_SUB',
-  useValue: new PubSub(),
+  ...(process.env.REDIS_ENABLED === '0' && {
+    provide: 'PUB_SUB',
+    useValue: new PubSub(),
+  }),
+  ...(process.env.REDIS_ENABLED === '1' && {
+    inject: [ConfigService],
+    provide: 'PUB_SUB',
+    useFactory: (configService: ConfigService) => {
+      const REDIS_URL = configService.get('REDIS_URL');
+
+      // const options = {
+      //   host: configService.get('REDIS_HOST'),
+      //   port: configService.get('REDIS_PORT'),
+      // };
+
+      return new RedisPubSub({
+        publisher: new Redis(REDIS_URL),
+        subscriber: new Redis(REDIS_URL),
+      });
+    },
+  }),
 };
-
-// export default {
-//   inject: [ConfigService],
-//   provide: 'PUB_SUB',
-//   useFactory: (configService: ConfigService) => {
-//     const options = {
-//       host: configService.get('REDIS_HOST'),
-//       port: configService.get('REDIS_PORT'),
-//     };
-
-//     return new RedisPubSub({
-//       publisher: new Redis(options),
-//       subscriber: new Redis(options),
-//     });
-//   },
-// },
