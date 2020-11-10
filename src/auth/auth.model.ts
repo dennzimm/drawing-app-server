ecd5da7930c01ec4fd299bc64f3701f3576bbbbc
Dennis Zimmermann <dennzimm@icloud.com>
2020-11-10 14:57:33 +0100

    Remove auth module

--- src/auth/auth.model.ts
+++
@@ -1,8 +0,0 @@
-import { ObjectType } from '@nestjs/graphql';
-import { Token } from '../users/token.model';
-import { User } from '../users/user.model';
-
-@ObjectType()
-export class Auth extends Token {
-  user: User;
-}
