a32cc4cbbfb40e05650bba06cc24f6834bcf1cb3
Dennis Zimmermann <dennzimm@icloud.com>
2020-11-23 20:06:48 +0100

    Update pubsub provider

--- src/pubsub/pubsub.provider.ts
+++ src/pubsub/pubsub.provider.ts
@@ -3,10 +3,7 @@ import { RedisPubSub } from 'graphql-redis-subscriptions';
 import * as Redis from 'ioredis';
 
 export const PubSubProvider = {
-  // {
-  //   provide: 'PUB_SUB',
   //   useValue: new PubSub(),
-  // }
   inject: [ConfigService],
   provide: 'PUB_SUB',
   useFactory: (configService: ConfigService) => {
