85d8f401bd546400ebc2fd0be81a5c198239ed13
Dennis Zimmermann <dennzimm@icloud.com>
2020-11-24 20:28:28 +0100

    Update app module documentation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,18 @@ import { PubSubModule } from '../pubsub/pubsub.module';
 import { AppResolver } from './app.resolver';
 import { AppService } from './app.service';
 
+/**
+ * The AppModule or "root module" is the starting point Nest uses to build
+ * the application graph - the internal data structure Nest uses to resolve
+ * module and provider relationships and dependencies.
+ *
+ * Within this module all other modules, services or providers are imported.
+ *
+ * (see https://docs.nestjs.com/modules)
+ *
+ * @export
+ * @class AppModule
+ */
 @Module({
   imports: [
     ConfigModule,
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,7 +1,20 @@
 import { Injectable } from '@nestjs/common';
 
+/**
+ * The AppService provides only one method: isOnline.
+ * In general, the business logic of resolvers is outsourced to service classes.
+ *
+ * @export
+ * @class AppService
+ */
 @Injectable()
 export class AppService {
+  /**
+   * This method can be used to check whether the server is reachable.
+   *
+   * @return {*}  {boolean}
+   * @memberof AppService
+   */
   isOnline(): boolean {
     return true;
   }
