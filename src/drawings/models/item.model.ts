8ab2d8d208a76d6380581785e28d9bbc0e69761b
Dennis Zimmermann <dennzimm@icloud.com>
2020-11-24 21:36:44 +0100

    Update drawing module models documentation

--- src/drawings/models/item.model.ts
+++ src/drawings/models/item.model.ts
@@ -1,6 +1,28 @@
 import { ObjectType, registerEnumType } from '@nestjs/graphql';
 import { Model } from './abstract/model.model';
 
+/**
+ * In the code first approach, we don't follow the typical process
+ * of creating our GraphQL schema by writing GraphQL SDL by hand.
+ * Instead, we use TypeScript decorators to generate the SDL from
+ * TypeScript class definitions. The @nestjs/graphql package reads
+ * the metadata defined through the decorators and automatically
+ * generates the schema for you.
+ *
+ * (see https://docs.nestjs.com/graphql/resolvers#object-types)
+ */
+
+/**
+ * ItemType Enum
+ *
+ * Enumeration types are a special kind of scalar that is restricted
+ * to a particular set of allowed values.
+ *
+ * (see https://docs.nestjs.com/graphql/enums)
+ *
+ * @export
+ * @enum {number}
+ */
 export enum ItemType {
   LAYER = 'LAYER',
   GROUP = 'GROUP',
@@ -12,6 +34,16 @@ registerEnumType(ItemType, {
   description: 'Paper Item Type (root level)',
 });
 
+/**
+ * Item Model
+ *
+ * This Item model represent a domain object that
+ * an application client might need to interact with.
+ *
+ * @export
+ * @class Item
+ * @extends {Model}
+ */
 @ObjectType()
 export class Item extends Model {
   name: string;
--- src/drawings/models/path.model.ts
+++ src/drawings/models/path.model.ts
@@ -1,6 +1,26 @@
 import { Field, Float, InputType, ObjectType } from '@nestjs/graphql';
 import { IsOptional } from 'class-validator';
 
+/**
+ * In the code first approach, we don't follow the typical process
+ * of creating our GraphQL schema by writing GraphQL SDL by hand.
+ * Instead, we use TypeScript decorators to generate the SDL from
+ * TypeScript class definitions. The @nestjs/graphql package reads
+ * the metadata defined through the decorators and automatically
+ * generates the schema for you.
+ *
+ * (see https://docs.nestjs.com/graphql/resolvers#object-types)
+ */
+
+/**
+ * Path Model
+ *
+ * This Path model represent a domain object that
+ * an application client might need to interact with.
+ *
+ * @export
+ * @class Path
+ */
 @InputType('PathInput')
 @ObjectType()
 export class Path {
