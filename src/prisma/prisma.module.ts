dada991c6910d1e70d52ee5570a18d96f63c92a0
Dennis Zimmermann <dennzimm@icloud.com>
2020-11-24 20:36:40 +0100

    Update prisma module documentation

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -7,6 +7,10 @@ import { PrismaService } from './prisma.service';
  * Prisma is used as an alternative to writing plain SQL, or using another database access
  * tool such as SQL query builders (like knex.js) or ORMs (like TypeORM and Sequelize).
  *
+ * We need to register the services with Nest so that it
+ * can perform the injection. We do this by adding the services
+ * to the providers array of the @Module() decorator.
+ *
  * (see https://docs.nestjs.com/recipes/prisma#prisma)
  *
  * @export
