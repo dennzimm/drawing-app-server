b6359b444318991c641e1f40d09b56f74e0396f2
Dennis Zimmermann <dennzimm@icloud.com>
2020-11-24 21:36:02 +0100

    Update drawing module enums documentation

--- src/drawings/common/enums/action.enum.ts
+++ src/drawings/common/enums/action.enum.ts
@@ -1,5 +1,18 @@
 import { registerEnumType } from '@nestjs/graphql';
 
+/**
+ * Enumeration types are a special kind of scalar that is restricted
+ * to a particular set of allowed values.
+ *
+ * (see https://docs.nestjs.com/graphql/enums)
+ */
+
+/**
+ * ActionType Enum
+ *
+ * @export
+ * @enum {number}
+ */
 export enum ActionType {
   PENCIL_DRAW = 'pencilDraw',
   BRUSH_DRAW = 'brushDraw',
--- src/drawings/common/enums/subscription.enum.ts
+++ src/drawings/common/enums/subscription.enum.ts
@@ -1,5 +1,18 @@
 import { registerEnumType } from '@nestjs/graphql';
 
+/**
+ * Enumeration types are a special kind of scalar that is restricted
+ * to a particular set of allowed values.
+ *
+ * (see https://docs.nestjs.com/graphql/enums)
+ */
+
+/**
+ * SubscriptionType Enum
+ *
+ * @export
+ * @enum {number}
+ */
 export enum SubscriptionType {
   DRAWING_ACTION = 'drawingActionPublished',
   ITEM_MUTATED = 'itemMutated',
