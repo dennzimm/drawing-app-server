12908bbc35fdd952d03a5b502b744838a25bb40f
Dennis Zimmermann <dennzimm@icloud.com>
2020-11-24 21:37:17 +0100

    Update drawing module documentation

--- src/drawings/drawings.module.ts
+++ src/drawings/drawings.module.ts
@@ -7,6 +7,18 @@ import { DrawingActionService } from './services/drawing-action.service';
 import { DrawingService } from './services/drawing.service';
 import { ItemService } from './services/item.service';
 
+/**
+ * This module contains all functions and GraphQL operations
+ * that should allow painting together in real-time.
+ *
+ * We need to register the services, resolvers and scalars
+ * with Nest so that it can perform the injection. We do this by
+ * adding the services, resolvers and scalars to the providers
+ * array of the @Module() decorator.
+ *
+ * @export
+ * @class DrawingsModule
+ */
 @Module({
   providers: [
     DrawingService,
