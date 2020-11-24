3cee7a7936092320708ee78e929af62720492650
Dennis Zimmermann <dennzimm@icloud.com>
2020-11-24 21:36:10 +0100

    Update drawing module interfaces documentation

--- src/drawings/common/interfaces/mutation.interface.ts
+++ src/drawings/common/interfaces/mutation.interface.ts
@@ -1,5 +1,19 @@
 import { MutationType } from '../enums/mutation.enum';
 
+/**
+ * An Interface is an abstract type that includes a certain
+ * set of fields that a type must include to implement the interface.
+ *
+ * (see https://docs.nestjs.com/graphql/interfaces)
+ */
+
+/**
+ * MutationNode Interface
+ *
+ * @export
+ * @interface MutationNode
+ * @template T
+ */
 export interface MutationNode<T> {
   node: T;
   variables: {
@@ -8,6 +22,13 @@ export interface MutationNode<T> {
   };
 }
 
+/**
+ * Mutation Interface
+ *
+ * @export
+ * @interface Mutation
+ * @template T
+ */
 export interface Mutation<T> {
   mutation: MutationType;
   payload: MutationNode<T>;
