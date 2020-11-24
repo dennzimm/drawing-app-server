a00e156004a68d5b6b4ccfbf8b83ff6b18b47ab3
Dennis Zimmermann <dennzimm@icloud.com>
2020-11-24 21:36:54 +0100

    Update drawing module resolvers documentation

--- src/drawings/resolvers/item.resolver.ts
+++ src/drawings/resolvers/item.resolver.ts
@@ -16,6 +16,20 @@ import {
   PublishItemMutationPayload,
 } from '../services/item.service';
 
+/**
+ * The ItemResolver provides GraphQL operations for Items.
+ * The business logic of this resolver is outsourced to the ItemService class.
+ *
+ * Resolvers provide the instructions for turning a GraphQL operation
+ * (a query, mutation, or subscription) into data. They return the same
+ * shape of data we specify in our schema -- either synchronously or as a
+ * promise that resolves to a result of that shape.
+ *
+ * (see https://docs.nestjs.com/graphql/resolvers)
+ *
+ * @export
+ * @class ItemResolver
+ */
 @Resolver((of) => Item)
 export class ItemResolver {
   constructor(
@@ -23,6 +37,15 @@ export class ItemResolver {
     private itemService: ItemService,
   ) {}
 
+  /**
+   * {Mutation} Create an Item.
+   *
+   * @param {CreateItemInput} { name, type, data }
+   * @param {UserIdInput} { userId }
+   * @param {DrawingNameInput} { drawingName }
+   * @return {*}
+   * @memberof ItemResolver
+   */
   @Mutation((returns) => Item, { nullable: true })
   async createItem(
     @Args('data') { name, type, data }: CreateItemInput,
@@ -58,6 +81,15 @@ export class ItemResolver {
     }
   }
 
+  /**
+   * {Mutation} Delete an Item.
+   *
+   * @param {DeleteItemInput} { name }
+   * @param {UserIdInput} { userId }
+   * @param {DrawingNameInput} { drawingName }
+   * @return {*}
+   * @memberof ItemResolver
+   */
   @Mutation((returns) => Item, { nullable: true })
   async deleteItem(
     @Args('data')
@@ -87,6 +119,18 @@ export class ItemResolver {
     }
   }
 
+  /**
+   * {Subscription} Subscribe to Item mutations.
+   *
+   * The following subscription handler takes care of subscribing
+   * to an ITEM_MUTATED event by calling the PubSub#asyncIterator.
+   * This method takes a single argument, the triggerName,
+   * which corresponds to an event topic name.
+   *
+   * @param {ItemMutatedArgs} itemMutated
+   * @return {*}  {(AsyncIterator<PubSubEngine, UserIdArgs & DrawingNameArgs>)}
+   * @memberof ItemResolver
+   */
   @Subscription((returns) => ItemMutation, {
     filter: (
       { itemMutated }: PublishItemMutationPayload,
