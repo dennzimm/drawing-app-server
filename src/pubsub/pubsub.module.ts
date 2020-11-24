eacdc0ab0b7aab6c332852044f1579237bd62f6e
Dennis Zimmermann <dennzimm@icloud.com>
2020-11-24 20:29:09 +0100

    Update pubsub module documentation

--- src/pubsub/pubsub.module.ts
+++ src/pubsub/pubsub.module.ts
@@ -1,6 +1,19 @@
 import { Global, Module } from '@nestjs/common';
 import { PubSubProvider } from './pubsub.provider';
 
+/**
+ * The PubSubModule is used to configure the PubSubProvider which provides the PubSub-API.
+ * Via this API, the AsyncIterator interface can be used to connect to the Subscription Manager,
+ * which is required for the implementation of subscriptions.
+ *
+ * The PubSub implementation also includes a mechanism that converts a specific PubSub event
+ * into a stream of AsyncIterator, which you can use with graphql subscriptions resolver.
+ *
+ * (https://www.apollographql.com/docs/graphql-subscriptions/setup/)
+ *
+ * @export
+ * @class PubSubModule
+ */
 @Global()
 @Module({
   providers: [PubSubProvider],
