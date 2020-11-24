12c85d89c4b9e297ba34c642472abf7e5345df70
Dennis Zimmermann <dennzimm@icloud.com>
2020-11-24 20:29:00 +0100

    Update prisma module documentation

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,6 +1,17 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
+/**
+ * Prisma is an open-source database toolkit.
+ * You can use it to query data from a database inside a Node.js or TypeScript application.
+ * Prisma is used as an alternative to writing plain SQL, or using another database access
+ * tool such as SQL query builders (like knex.js) or ORMs (like TypeORM and Sequelize).
+ *
+ * (see https://docs.nestjs.com/recipes/prisma#prisma)
+ *
+ * @export
+ * @class PrismaModule
+ */
 @Global()
 @Module({
   providers: [PrismaService],
